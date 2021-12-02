b3395435c21e42bb7f9defeeabea7a33020414c3
Shivam <gupta.shivam@neosoftmail.com>
2021-12-02 11:15:45 +0530

    Discount microservice developed

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,55 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+
+        readonly IDiscountRepository _repository;
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{productName}",Name ="GetDiscount")]
+        [ProducesResponseType(typeof(Coupon),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _repository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost("CreateDiscount")]
+        public async Task<ActionResult<bool>> CreateDiscount(Coupon coupon)
+        {
+            await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut("UpdateDiscount")]
+        public async Task<ActionResult<bool>> UpdateDiscount(Coupon coupon)
+        {
+            await _repository.UpdateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpDelete("DeleteDiscount")]
+        public async Task<ActionResult<string>> DeleteDiscount(string productName)
+        {
+            var response = await _repository.DeleteDiscount(productName);
+            if (!response)
+                return $"Error in  deleting {productName} coupon";
+            return $"{productName} deleted successfully";
+
+        }
+    }
+}
